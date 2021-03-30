e82dd7427e052a147153a9819bd66a7f148e85fc
Christoph Ortner <c.ortner@warwick.ac.uk>
2021-03-30 13:52:01 -0700

    first sketch

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # ACEbase
 
 [![Build Status](https://github.com/cortner/ACEbase.jl/workflows/CI/badge.svg)](https://github.com/cortner/ACEbase.jl/actions)
+
+Base definitions and universal methods primarily intended for `ACEsuit` and `JuLIP`, but may be more generally useful.
+
+## License
+
+`ACEbase.jl` is © 2021, Christoph Ortner
+
+`ACEbase.jl` is published and distributed under the [Academic Software License v1.0 (ASL).](ASL.md)
+
+`ACEbase.jl` is distributed in the hope that it will be useful for non-commercial academic research, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the ASL for more details.
+
+You should have received a copy of the ASL along with this program; if not, write to Christoph Ortner, christophortner0@gmail.com. It is also published at [https://github.com/gabor1/ASL/blob/main/ASL.md](https://github.com/gabor1/ASL/blob/main/ASL.md).
+
+You may contact the original licensor at `christophortner0@gmail.com`.
--- src/ACEbase.jl
+++ src/ACEbase.jl
@@ -1,5 +1,46 @@
 module ACEbase
 
-# Write your package code here.
+using Reexport
+
+
+function alloc_B end
+function alloc_dB end
+function fltype end
+function alloc_temp end
+function alloc_temp_d end
+function evaluate! end
+function evaluate_d! end
+function write_dict end
+function read_dict end
+
+abstract type AbstractBasis end
+abstract type ACEBasis <: AbstractBasis end
+abstract type OneParticleBasis{T} <: ACEBasis end
+abstract type ScalarACEBasis <: ACEBasis end
+
+alloc_temp(::ACEBasis, args...) = nothing
+alloc_temp_d(::ACEBasis, args...) = nothing
+
+evaluate(basis::ACEBasis, args...) =
+      evaluate!(alloc_B(basis), alloc_temp(basis), basis, args...)
+
+evaluate_d(basis::ACEBasis, args...) =
+      evaluate_d!(alloc_dB(basis), alloc_temp_d(basis), basis, args...)
+
+
+# Some overloading to enable AD for some cases
+#   TODO -> this needs to be extended..
+
+evaluate(basis::ScalarACEBasis, args...) =
+      evaluate!(alloc_B(basis, args...), alloc_temp(basis), basis, args...)
+
+evaluate_d(basis::ScalarACEBasis, args...) =
+      evaluate_d!(alloc_B(basis, args...), alloc_temp(basis), basis, args...)
+
+
+
+include("fio.jl")
+@reexport using ACEbase.FIO
+
 
 end
