245fb3681f3c2d32bd8c3cd8583544e4502a79f7
Christoph Ortner <christohortner@gmail.com>
2023-05-03 15:23:35 -0700

    revert to evaluate_dd, remove aquire

--- src/ACEbase.jl
+++ src/ACEbase.jl
@@ -10,18 +10,21 @@ include("def.jl")
 # restricted in any way. 
 function evaluate end
 function evaluate_d end
-function evaluate_d2 end
+function evaluate_dd end
 function evaluate_ed end
 function evaluate_ed2 end
 function evaluate! end
 function evaluate_d! end
-function evaluate_d2! end
+function evaluate_dd! end
 function evaluate_ed! end
+function evaluate_ed2! end
 
-# these are useful functions to share across ACEsuit packages as well since 
+# these could be useful functions to share across ACEsuit packages as well since 
 # there are many different ways how one can allocate and release memory. 
-function acquire! end 
-function release! end 
+# For now, we will make them owned by ObjectPools.jl so that that package 
+# doesn't have to depend on ACEbase. 
+# function acquire! end 
+# function release! end 
 
 # This is a helpful little utility that we don't know where else to put.
 """
@@ -31,6 +34,7 @@ allfieldsequal(x1, x2) =
       all( getfield(x1, sym) == getfield(x2, sym)
            for sym in union(fieldnames(typeof(x1)), fieldnames(typeof(x2))) )
 
+@deprecate _allfieldsequal(args...) allfieldsequal(args...)
 
 # This creates a sub-module FIO
 # FIO via JSON and YAML functionality is collected here. We don't trust 
